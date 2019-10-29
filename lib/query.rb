2540fa42daed859dec98ac6cc2708ecbfa77640a
Kevin Evans <mkevinevans@gmail.com>
2019-10-28 20:17:18 -0400

    adds query class and test to ensure search term is present

---
+++ lib/query.rb
@@ -0,0 +1,12 @@
+class Query
+
+    def initialize(search_term:)
+
+        @search_term = search_term
+    end
+
+    def search_term
+        @search_term
+    end
+end
+
---
+++ spec/query_spec.rb
@@ -0,0 +1,19 @@
+require 'query'
+
+RSpec.describe 'a search query' do
+
+    def query(params ={})
+        default = { 
+            search_term: "boston molasses disaster"
+    }
+        Query.new(default.merge(params))
+    end
+
+    it 'has a default search term if none is provided (in testing only)' do
+        raise unless query().search_term == "boston molasses disaster"
+    end
+
+    it 'accepts a search term' do
+        raise unless query(search_term: "dogs").search_term == "dogs"
+    end
+end
