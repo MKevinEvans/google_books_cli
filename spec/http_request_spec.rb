d42c0c1b58efe298cd61a8c0dafd32be2caf71fa
Kevin Evans <mkevinevans@gmail.com>
2019-11-08 15:28:34 -0500

    changes http_request_spec to test for actual return values instead of return types

--- spec/http_request_spec.rb
+++ spec/http_request_spec.rb
@@ -9,20 +9,25 @@ RSpec.describe 'an HTTP request' do
         Http_request.new(default.merge(params))
     end
 
+    def make_test_query
+        Query.new(search_term: "dogs")
+    end
+    let (:test_query) {make_test_query}
+
     it 'accepts a query' do
-        expect(http_request(query: Query.new(search_term: "dogs")).query).to be_an_instance_of(Query)
+        expect("dogs").to eq(http_request(query: test_query).query.search_term)
     end
 
     it 'has a default query if none is provided (for testing only)' do
-        expect(http_request().query).to be_an_instance_of(Query)
+        expect("boston molasses disaster").to eq(http_request().query.search_term)
     end
 
-    it 'contains a base url' do
-        expect(http_request().base_url).to be_an_instance_of(String)
+    it 'contains google books api\'s base url' do
+        expect("https://www.googleapis.com/books/v1/volumes?q=").to eq(http_request().base_url)
     end
 
     it 'combines the base url with the search term' do
-        expect(http_request().final_url).to eq("https://www.googleapis.com/books/v1/volumes?q=boston molasses disaster")
+        expect("https://www.googleapis.com/books/v1/volumes?q=boston molasses disaster").to eq(http_request().final_url)
     end
 
 end
